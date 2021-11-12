8dd5353b262f5f951c0235da3efb9b8d52cb7b94
mahabubimon <mahabubemon24@gmail.com>
2021-11-12 17:30:08 +0600

    make it better

--- index.js
+++ index.js
@@ -12,9 +12,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@randomdb.rfcve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//  Create Client in MongoDB
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // Server Run Function Expression
 const run = async () => {
   try {
+    await client.connect();
+    const database = client.db("chasmish-hero");
+    const glasse
+    sCollection = database.collection("glasses");
+    // const ordersCollection = database.collection("orders");
+
+    // Get API - Glasses
+    app.get("/glasses", async (req, res) => {
+      const cursor = glassesCollection.find({});
+      const glasses = await cursor.toArray();
+      res.send(glasses);
+    });
   } catch {
   } finally {
     // await client.close();

5d79edb7e3f376ec9873fce0cda889b21671c578
mahabubimon <mahabubemon24@gmail.com>
2021-11-15 20:38:18 +0600

    server site complete

---
+++ README.md
@@ -0,0 +1,47 @@
+# Chasmish Hero Web App
+
+It is a single page application made with ReactJS, Bootstrap, ...
+
+## Live Demo of this website :
+
+### [Chasmish Hero Live Site](https://chasmish-hero.web.app/)
+
+
+### [Chasmish Hero Server Site](https://chasmish-hero.web.app/)
+
+- [Live Link:] https://chasmish-hero.web.app/
+
+## Database Collection
+
+- Users Collection
+- Glasses (Products) Collection
+- Review Collection
+- Orders Collection
+
+## Technologies
+
+- React Router for Navigation
+- React Bootstrap for Responsive CSS
+- React Icons for Icons
+- React Use Hook Form
+- Firebase for Authentication
+- Axios
+- Express for Server App
+- MongoDB for Database
+- Heroku for Server Host
+
+[![React Badge](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)](https://github.com/mahabubimon)
+[![Tailwind Badge](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)](https://github.com/mahabubimon)
+[![Firebase Badge](https://img.shields.io/badge/Firebase-FFCB2B?style=for-the-badge&logo=firebase&logoColor=white)](https://github.com/mahabubimon)
+[![React Router Badge](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)](https://github.com/mahabubimon)
+[![Git Badge](https://img.shields.io/badge/git-f34f29?style=for-the-badge&logo=git&logoColor=white)](https://github.com/mahabubimon)
+[![Yarn Badge](https://img.shields.io/badge/yarn-0078D6?style=for-the-badge&logo=yarn&logoColor=white)](https://github.com/mahabubimon)
+[![Vercel Badge](https://img.shields.io/badge/vercel-000?style=for-the-badge&logo=vercel&logoColor=white)](https://github.com/mahabubimon)
+[![Vs Code Badge](https://img.shields.io/badge/Visual_Studio_Code-0078D6?style=for-the-badge&logo=visualstudiocode&logoColor=white)](https://github.com/mahabubimon)
+
+## 🚀 Connect with me
+
+[![Facebook Badge](https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white)](https://www.facebook.com/mahabubimonbd/)
+[![Instagram Badge](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/mahabubimon/)
+[![Linkedin Badge](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/mahabubimon)
+[![Github Badge](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/mahabubimon)
--- index.js
+++ index.js
@@ -8,32 +8,134 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middle Wire
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB URI
+// MongoDB Connection URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@randomdb.rfcve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-//  Create Client in MongoDB
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Server Run Function Expression
+// DataBase Connection Function
 const run = async () => {
   try {
     await client.connect();
     const database = client.db("chasmish-hero");
+    const usersCollection = database.collection("users");
     const glassesCollection = database.collection("glasses");
-    // const ordersCollection = database.collection("orders");
+    const ordersCollection = database.collection("orders");
+    const reviewsCollection = database.collection("reviews");
+
+    // Post API - User Add
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // Get API - Users
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    //  Update API - User
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // Get API - Admin
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // Update API- Add New Admin
+    app.put("/addAdmin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
     // Get API - Glasses
     app.get("/glasses", async (req, res) => {
       const cursor = glassesCollection.find({});
       const glasses = await cursor.toArray();
       res.send(glasses);
+    });
+
+    // Find API - Glass
+    app.get("/glasses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await glassesCollection.findOne(query);
+      res.json(result);
+    });
+
+    // Post API - Glasses
+    app.post("/glasses", async (req, res) => {
+      const glass = req.body;
+      const result = await glassesCollection.insertOne(glass);
+      res.json(result);
+    });
+
+    // Post API- Orders
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+
+    // Get API - orders
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // Delete API - Order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // Post API- Reviews
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.json(result);
+    });
+
+    // Get API - Reviews
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
     });
   } catch {
   } finally {
